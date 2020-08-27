0eb711e16150c4e59ac953ca04ed42d94e4fc5fd
Rohan Jain <rohan19095@iiitd.ac.in>
2020-08-27 23:05:04 +0530

    Added Makefile and add.asm

--- assignment02/prog-add.c
+++ assignment02/prog-add.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
-extern void _add_two(int64_t, int64_t);
+extern void _add(int64_t, int64_t);
 
 int main(int argc, char **argv)
 {
     int64_t x, y;
-    scanf("%" PRId64 "%" PRId64, &x, &y);
-    _add_two(x, y);
+    // scanf("%" PRId64 "%" PRId64, &x, &y);
+    x = atoll(argv[1]);
+    y = atoll(argv[2]);
+    _add(x, y);
 }
