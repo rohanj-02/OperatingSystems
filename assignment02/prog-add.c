aa1038c5052438f4778d04e3bc2134ad201e1ccf
Rohan Jain <rohan19095@iiitd.ac.in>
2020-08-30 17:50:43 +0530

    Changed prog-add acc. to updated assignment

--- assignment02/prog-add.c
+++ assignment02/prog-add.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
-extern void _add(int64_t, int64_t);
+
+extern int64_t _add(int64_t, int64_t);
 
 int main(int argc, char **argv)
 {
     int64_t x, y;
-    // scanf("%" PRId64 "%" PRId64, &x, &y);
-    x = atoll(argv[1]);
-    y = atoll(argv[2]);
-    _add(x, y);
+    printf("Enter two space seperated integers: ");
+    scanf("%" PRId64 "%" PRId64, &x, &y);
+    // x = atoll(argv[1]);
+    // y = atoll(argv[2]);
+    printf("%" PRId64 "\n", _add(x, y));
+    return 0;
 }
