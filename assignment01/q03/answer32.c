cb2cc358e9e548d754a81d535cb8860c8c5103a0
Rohan Jain <rohan19095@iiitd.ac.in>
2020-09-17 23:21:01 +0530

    Quiz 1 Question 3

---
+++ assignment01/q03/answer32.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+    char x[64];
+    int *ptr = (int*)x;
+    for(int i = 0; i < 15; i++){
+        *ptr = i;
+        ptr++;
+    }
+    int i2 = 0x7FFFFFFF;
+    *ptr = i2;
+    int *itr = (int*)x;
+    for(int i = 0; i < 16; i++){
+        printf("%d ", *itr);
+        itr++;
+    }
+}
---
+++ assignment01/q03/answer64.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main(){
+    char x[64];
+    long int *ptr = (long int*)x;
+    for(int i = 0; i < 7; i++){
+        *ptr = i;
+        ptr++;
+    }
+    long int i2 = 0x7FFFFFFFFFFFFFFF;
+    *ptr = i2;
+    long int *itr = (long int*)x;
+    for(int i = 0; i < 8; i++){
+        printf("%ld ", *itr);
+        itr++;
+    }
+
+    // if(x+64 == itr){
+    //     printf("True");
+    // }
+    // printf("%p", x + 64);
+    // printf("%p", itr);
+    // for(int i = 0; i < 64; i++){
+    //     printf("%d ", (int)x[i]);
+    // }
+}
