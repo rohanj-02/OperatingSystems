3df04b7628e6145fbe2330e7630278dd3cc963f4
Rohan Jain <rohan19095@iiitd.ac.in>
2020-09-24 21:12:19 +0530

    Shell: InputParser Complete

--- assignment01/exercise1.2/main.c
+++ assignment01/exercise1.2/main.c
@@ -6,93 +6,74 @@
 #include <string.h>
 #include <stdbool.h>
 
-size_t NUMBER_OF_ARGUMENTS = 10;
+int NUMBER_OF_ARGUMENTS = 10;
 
-char **getInput(char **inputArr)
+char **getArguments()
 {
-	char x;
-	int counter = 0;
-	int smallCounter = 0;
-	// int CURRENT_SIZE = sizeof(inputArr) / sizeof(char);
-	// printf("%d", NUMBER_OF_ARGUMENTS);
-	// size_t NUMBER_OF_ARGUMENTS = 5;
-	bool flag = true;
-	while (x = getchar())
+	bool isInput = true;
+	char **input = (char **)calloc(10, sizeof(char *));
+	int currentNumber = 0;
+	while (isInput)
 	{
-		if (x == '\n')
+		char x;
+		int length = 0;
+		int CURRENT_SIZE = 20;
+		char *string = (char *)malloc(CURRENT_SIZE);
+		bool flag = true;
+		while (x = getchar())
 		{
-			break;
-		}
-		else if (x == 32)
-		{
-			inputArr[counter][smallCounter] = '\0';
-			smallCounter = 0;
-			counter++;
-			if (counter >= NUMBER_OF_ARGUMENTS)
+			if (!isInput)
 			{
-				int newSize = 2 * NUMBER_OF_ARGUMENTS;
-				newSize = newSize * sizeof(char *);
-				char **newp = (char **)realloc(inputArr, newSize);
-				inputArr = newp;
-				NUMBER_OF_ARGUMENTS *= 2;
+				break;
 			}
-			flag = true;
-		}
-		else
-		{
-			if (flag)
+			if (x == '"')
 			{
-				flag = false;
-				char temp[2] = "\0";
-				temp[0] = x;
-				inputArr[counter][smallCounter] = x;
-				// strcpy(inputArr[counter], temp);
+				flag = !flag;
+			}
+			if (x == '\n')
+			{
+				isInput = false;
+			}
+			if ((x == ' ' && flag) || !isInput)
+			{
+				input[currentNumber] = string;
+				currentNumber++;
+				if (currentNumber >= NUMBER_OF_ARGUMENTS)
+				{
+					char **temp3 = realloc(input, 2 * NUMBER_OF_ARGUMENTS * sizeof(char *));
+					NUMBER_OF_ARGUMENTS *= 2;
+					input = temp3;
+				}
+				break;
 			}
 			else
 			{
-				// inputArr[counter] += x;
-				char str[2] = "\0";
-				str[0] = x;
-				inputArr[counter][smallCounter] = x;
-				// strcat(inputArr[counter], str);
+				char temp[2] = "\0";
+				temp[0] = x;
+				strcat(string, temp);
+				length++;
+				if (length >= CURRENT_SIZE)
+				{
+					char *temp2 = realloc(string, 2 * CURRENT_SIZE);
+					CURRENT_SIZE *= 2;
+					string = temp2;
+				}
 			}
-			smallCounter++;
-			// strcpy(inputArr[counter], inputArr[counter] + x);
 		}
 	}
-	// printf("Loop completed bro!");
-	return inputArr;
+	NUMBER_OF_ARGUMENTS = currentNumber;
+	return input;
 }
 
-char *getOneArgument()
+void executeCommands(char **input)
 {
-	char x;
-	int length = 0;
-	int CURRENT_SIZE = 20;
-	char *string = (char *)malloc(CURRENT_SIZE);
-	bool flag = true;
-	while (x = getchar())
+	if (isInternal(input[0]))
 	{
-		if (x == '"')
-		{
-			flag = !flag;
-		}
-		if ((x == ' ' || x == '\n') && flag)
-		{
-			return string;
-		}
-		else
-		{
-			char temp[2] = "\0";
-			temp[0] = x;
-			strcat(string, temp);
-			length++;
-			if (length >= CURRENT_SIZE)
-			{
-				char *temp2 = realloc(string, 2 * CURRENT_SIZE);
-				string = temp2;
-			}
-		}
+		handleInternal(input);
+	}
+	else
+	{
+		handleExternal(input);
 	}
 }
 
@@ -107,41 +88,15 @@ int main(int argc, char *argv[])
 		5. Handle errors that can happen 
 		6. Check how you can implement only the specific external commands and not all external commands
 	*/
-	// system("clear");
-	// int initialSize = NUMBER_OF_ARGUMENTS * sizeof(char *);
-	// char **x = (char **)malloc(initialSize);
-	// for (int i = 0; i < initialSize; i++)
-	// {
-	// 	x[i] = (char *)malloc(100);
-	// }
-	// x = getInput(x);
-	// printf("%ld", NUMBER_OF_ARGUMENTS);
-	// for (int i = 0; i < NUMBER_OF_ARGUMENTS; i++)
-	// {
-	// 	printf("%s\n", x[i]);
-	// }
-	// free(x);
-	char **input = (char **)calloc(10, sizeof(char *));
-	for (int i = 0; i < 10; i++)
+	while (1)
 	{
-		// input[i] = (char *)calloc(10, 1);
-		// scanf("%s", input[i]);
-		input[i] = getOneArgument();
-		// strcpy(input[i], "YOLO");
-		// printf("Pointer:%p Value:%s Length:%ld \n", *input[i], input[i], strlen(input[i]));
-	}
-	char **newptr = (char **)realloc(input, 20 * sizeof(char *));
-	input = newptr;
-	for (int i = 10; i < 20; i++)
-	{
-		input[i] = (char *)calloc(10, 1);
-		strcpy(input[i], "SECOND?");
-	}
-	for (int i = 0; i < 20; i++)
-	{
-		printf("Pointer:%p Value:%s Length:%ld\n", *input[i], input[i], strlen(input[i]));
+		char **input = getArguments();
+		for (int i = 0; i < NUMBER_OF_ARGUMENTS; i++)
+		{
+			printf("Pointer:%p Value:%s Length:%ld\n", *input[i], input[i], strlen(input[i]));
+		}
+		free(input);
 	}
 
-	free(input);
 	return 0;
 }
---
+++ assignment01/exercise1.2/prevParser.c
@@ -0,0 +1,89 @@
+// size_t NUMBER_OF_ARGUMENTS = 10;
+
+// char **getInput(char **inputArr)
+// {
+// 	char x;
+// 	int counter = 0;
+// 	int smallCounter = 0;
+// 	// int CURRENT_SIZE = sizeof(inputArr) / sizeof(char);
+// 	// printf("%d", NUMBER_OF_ARGUMENTS);
+// 	// size_t NUMBER_OF_ARGUMENTS = 5;
+// 	bool flag = true;
+// 	while (x = getchar())
+// 	{
+// 		if (x == '\n')
+// 		{
+// 			break;
+// 		}
+// 		else if (x == 32)
+// 		{
+// 			inputArr[counter][smallCounter] = '\0';
+// 			smallCounter = 0;
+// 			counter++;
+// 			if (counter >= NUMBER_OF_ARGUMENTS)
+// 			{
+// 				int newSize = 2 * NUMBER_OF_ARGUMENTS;
+// 				newSize = newSize * sizeof(char *);
+// 				char **newp = (char **)realloc(inputArr, newSize);
+// 				inputArr = newp;
+// 				NUMBER_OF_ARGUMENTS *= 2;
+// 			}
+// 			flag = true;
+// 		}
+// 		else
+// 		{
+// 			if (flag)
+// 			{
+// 				flag = false;
+// 				char temp[2] = "\0";
+// 				temp[0] = x;
+// 				inputArr[counter][smallCounter] = x;
+// 				// strcpy(inputArr[counter], temp);
+// 			}
+// 			else
+// 			{
+// 				// inputArr[counter] += x;
+// 				char str[2] = "\0";
+// 				str[0] = x;
+// 				inputArr[counter][smallCounter] = x;
+// 				// strcat(inputArr[counter], str);
+// 			}
+// 			smallCounter++;
+// 			// strcpy(inputArr[counter], inputArr[counter] + x);
+// 		}
+// 	}
+// 	// printf("Loop completed bro!");
+// 	return inputArr;
+// }
+
+// TODO MAIN
+// system("clear");
+// int initialSize = NUMBER_OF_ARGUMENTS * sizeof(char *);
+// char **x = (char **)malloc(initialSize);
+// for (int i = 0; i < initialSize; i++)
+// {
+// 	x[i] = (char *)malloc(100);
+// }
+// x = getInput(x);
+// printf("%ld", NUMBER_OF_ARGUMENTS);
+// for (int i = 0; i < NUMBER_OF_ARGUMENTS; i++)
+// {
+// 	printf("%s\n", x[i]);
+// }
+// free(x);
+// char **input = (char **)calloc(10, sizeof(char *));
+// for (int i = 0; i < 10; i++)
+// {
+// 	// input[i] = (char *)calloc(10, 1);
+// 	// scanf("%s", input[i]);
+// 	input[i] = getOneArgument();
+// 	// strcpy(input[i], "YOLO");
+// 	// printf("Pointer:%p Value:%s Length:%ld \n", *input[i], input[i], strlen(input[i]));
+// }
+// char **newptr = (char **)realloc(input, 20 * sizeof(char *));
+// input = newptr;
+// for (int i = 10; i < 20; i++)
+// {
+// 	input[i] = (char *)calloc(10, 1);
+// 	strcpy(input[i], "SECOND?");
+// }
