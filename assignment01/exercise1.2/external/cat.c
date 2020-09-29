410d0362891b1d29d8d4bb41a657aff918c98f9d
Rohan Jain <rohan19095@iiitd.ac.in>
2020-09-29 13:33:32 +0530

    Shell: cat Implemented

--- assignment01/exercise1.2/external/cat.c
+++ assignment01/exercise1.2/external/cat.c
@@ -2,11 +2,48 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define length 256
 
 int main(int argc, char *argv[])
 {
 	int fileDescriptor;
-	for (int i = 1; i < argc; i++)
+	int countLineNumber = 2;
+	int startIndex = 1;
+	bool isLineNumber = false;
+	bool isLineEnd = false;
+	if (strcmp(argv[1], "--help") == 0)
+	{
+		printf("Implements -n and -E flag !");
+		//TODO Add documentation
+		return 0;
+	}
+	if (strcmp(argv[1], "-E") == 0)
+	{
+		isLineEnd = true;
+		startIndex = 2;
+	}
+	if (strcmp(argv[1], "-n") == 0)
+	{
+		isLineNumber = true;
+		startIndex = 2;
+	}
+	if ((strcmp(argv[1], "-nE") == 0) || (strcmp(argv[1], "-En") == 0))
+	{
+		isLineEnd = true;
+		isLineNumber = true;
+		startIndex = 2;
+	}
+	if (isLineNumber)
+	{
+		char temp = '1';
+		write(STDOUT_FILENO, &temp, 1);
+		temp = '\t';
+		write(STDOUT_FILENO, &temp, 1);
+	}
+	for (int i = startIndex; i < argc; i++)
 	{
 		if ((fileDescriptor = open(argv[i], O_RDONLY)) < 0)
 		{
@@ -17,8 +54,31 @@ int main(int argc, char *argv[])
 			char oneChar;
 			while (read(fileDescriptor, &oneChar, 1))
 			{
-				write(STDOUT_FILENO, &oneChar, 1);
+				if (oneChar == '\n')
+				{
+					if (isLineEnd)
+					{
+						char temp = '$';
+						write(STDOUT_FILENO, &temp, 1);
+					}
+					write(STDOUT_FILENO, &oneChar, 1);
+					if (isLineNumber)
+					{
+						int printLen = snprintf(NULL, 0, "%d", countLineNumber);
+						char str[length];
+						sprintf(str, "%d", countLineNumber);
+						write(STDOUT_FILENO, &str, printLen);
+						countLineNumber++;
+						oneChar = '\t';
+						write(STDOUT_FILENO, &oneChar, 1);
+					}
+				}
+				else
+				{
+					write(STDOUT_FILENO, &oneChar, 1);
+				}
 			}
+
 			oneChar = '\n';
 			write(STDOUT_FILENO, &oneChar, 1);
 			close(fileDescriptor);
