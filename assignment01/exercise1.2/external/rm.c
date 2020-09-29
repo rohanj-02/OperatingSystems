cdb6bb2bc45ca10643533bacfab2e68c80883f56
Rohan Jain <rohan19095@iiitd.ac.in>
2020-09-29 15:30:28 +0530

    Shell: rm implemented

--- assignment01/exercise1.2/external/rm.c
+++ assignment01/exercise1.2/external/rm.c
@@ -1,18 +1,90 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
 
 // TODO Change code s.t. it cannot remove directory.. It needs a flag to do it.
+// -i -d -r to see
 
 int main(int argc, char *argv[])
 {
-	for (int i = 1; i < argc; i++)
+	bool isPrompt = false;
+
+	if (strcmp(argv[1], "--help") == 0)
 	{
-		if (remove(argv[i]) == 0)
+		printf("Can implement -i and -d");
+		//TODO Write documentation
+	}
+	if ((strcmp(argv[1], "-i") == 0) || (strcmp(argv[1], "-id") == 0) || (strcmp(argv[1], "-di") == 0))
+	{
+		isPrompt = true;
+	}
+	if ((strcmp(argv[1], "-d") == 0) || (strcmp(argv[1], "-id") == 0) || (strcmp(argv[1], "-di") == 0))
+	{
+		for (int i = 2; i < argc; i++)
 		{
-			printf("Succesfully deleted %s\n", argv[i]);
+			bool flag = false;
+			if (isPrompt)
+			{
+				printf("Delete %s?(y/n) ", argv[i]);
+				char x;
+				scanf("%c", &x);
+				flag = x == 'y';
+			}
+			else
+			{
+				flag = true;
+			}
+			if (flag)
+			{
+				if (remove(argv[i]) == 0)
+				{
+					printf("Succesfully deleted %s\n", argv[i]);
+				}
+				else
+				{
+					char error[256];
+					sprintf(error, "rm: cannot remove %s", argv[i]);
+					perror(error);
+				}
+			}
 		}
-		else
+	}
+	else
+	{
+		int startIndex = 1;
+		if (isPrompt)
 		{
-			printf("File %s doesn't exist\n", argv[i]);
+			startIndex = 2;
+		}
+		for (int i = startIndex; i < argc; i++)
+		{
+			bool flag = false;
+			if (isPrompt)
+			{
+				printf("Delete %s?(y/n) ", argv[i]);
+				char x;
+				scanf("%c", &x);
+				flag = x == 'y';
+			}
+			else
+			{
+				flag = true;
+			}
+			if (flag)
+			{
+				if (unlink(argv[i]) == 0)
+				{
+					printf("Succesfully deleted %s\n", argv[i]);
+				}
+				else
+				{
+					char error[256];
+					sprintf(error, "rm: cannot remove %s", argv[i]);
+					perror(error);
+				}
+			}
 		}
 	}
 
