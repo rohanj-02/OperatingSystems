ac9de3f3eb04a2afa51ac6fdef433d549f856c4e
Rohan Jain <rohan19095@iiitd.ac.in>
2020-10-28 06:39:44 -0700

    Line input in test.c

--- assignment02/Q2/test.c
+++ assignment02/Q2/test.c
@@ -1,26 +1,33 @@
-/* Name: Rohan Jain
-   Roll_Number: 2019095 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
+#define SYSCALL_NUMBER 440
+#define PATH_LIMIT 256
+
 int main(int argc, char **argv)
 {
-	char user_filename[256];
+	char user_filename[PATH_LIMIT];
 	pid_t pid;
 	long int sys_return;
 
 	printf("Enter pid(-1 to send pid of this process): ");
 	scanf("%d", &pid);
 	printf("Enter filename: ");
-	scanf("%s", user_filename);
+	fgets(user_filename, PATH_LIMIT, stdin); 
+	fgets(user_filename, PATH_LIMIT, stdin);
+	for(int i = 0; user_filename[i] != '\0'; i++){
+		if(user_filename[i+1] == '\0'){
+			user_filename[i] = '\0';
+		}
+	}
+
 	
 	if(pid == -1){
-		sys_return = syscall(440, getpid(), user_filename);
+		sys_return = syscall(SYSCALL_NUMBER, getpid(), user_filename);
 	}
 	else{
-		sys_return = syscall(440, pid, user_filename);		
+		sys_return = syscall(SYSCALL_NUMBER, pid, user_filename);		
 	}
 	if(sys_return != 0){
 		perror("Error: sh_task_info");
