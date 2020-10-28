bfc7bd18a23703d1156cc7266d4bfa28b6621046
Rohan Jain <rohan19095@iiitd.ac.in>
2020-10-28 19:14:59 +0530

    Formatting changes

--- assignment02/Q2/test.c
+++ assignment02/Q2/test.c
@@ -14,22 +14,26 @@ int main(int argc, char **argv)
 	printf("Enter pid(-1 to send pid of this process): ");
 	scanf("%d", &pid);
 	printf("Enter filename: ");
-	fgets(user_filename, PATH_LIMIT, stdin); 
 	fgets(user_filename, PATH_LIMIT, stdin);
-	for(int i = 0; user_filename[i] != '\0'; i++){
-		if(user_filename[i+1] == '\0'){
+	fgets(user_filename, PATH_LIMIT, stdin);
+	for (int i = 0; user_filename[i] != '\0'; i++)
+	{
+		if (user_filename[i + 1] == '\0')
+		{
 			user_filename[i] = '\0';
 		}
 	}
 
-	
-	if(pid == -1){
+	if (pid == -1)
+	{
 		sys_return = syscall(SYSCALL_NUMBER, getpid(), user_filename);
 	}
-	else{
-		sys_return = syscall(SYSCALL_NUMBER, pid, user_filename);		
+	else
+	{
+		sys_return = syscall(SYSCALL_NUMBER, pid, user_filename);
 	}
-	if(sys_return != 0){
+	if (sys_return != 0)
+	{
 		perror("Error: sh_task_info");
 		return -1;
 	}
