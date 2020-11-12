1f66f1570bf082e1643e3c604fb5464e24af00ae
Rohan Jain <rohan19095@iiitd.ac.in>
2020-11-12 01:57:30 -0800

    Change get_time logic and code cleanup

--- assignment03/Q1/test.c
+++ assignment03/Q1/test.c
@@ -9,14 +9,20 @@
 #define SYSCALL_NUMBER 440
 #define LOOP_END 10000000000
 
+/* Returns time taken to run a loop from 0 to LOOP_END */
 double get_time()
 {
 	struct timeval start_time, end_time;
 	gettimeofday(&start_time, NULL);
 	for(long int i = 0; i < LOOP_END; i++){}
 	gettimeofday(&end_time, NULL);
-	double timeDiff = end_time.tv_sec - start_time.tv_sec + 0.000001*(end_time.tv_usec - start_time.tv_usec);
-	return timeDiff;
+	long long diff_usec = end_time.tv_usec - start_time.tv_usec;
+	if( diff_usec < 0){
+		diff_usec += 1000000;
+		end_time.tv_sec -= 1;
+	}
+	return (end_time.tv_sec - start_time.tv_sec + 0.000001*diff_usec);
+	
 }
 
 int main(int argc, char **argv)
@@ -29,46 +35,36 @@ int main(int argc, char **argv)
 	scanf("%d", &pid);
 	printf("Enter soft realtime requirements: ");
 	scanf("%lld", &srt_val);
-	
-	
+		
 	fork_pid = fork();
 	if(fork_pid == -1){
-		perror("Fork error");
+		perror("fork: error");
 		exit(1);
 	}
 	else if(fork_pid == 0){
-		sys_return = syscall(SYSCALL_NUMBER, getpid(), srt_val * 1000000000);
+		if(pid == -1){
+			pid = getpid();
+		}
+		// Converting seconds to nanoseconds because syscall takes nanoseconds as param
+		sys_return = syscall(SYSCALL_NUMBER, pid, srt_val * 1000000000);
 		if (sys_return != 0)
 		{
-			perror("Error: rtnice");
+			perror("rtnice: error");
 			return -1;
 		}
 		else{
-			printf("Realtime time: %f\n", get_time());
+			printf("Time taken with soft realtime requirements: %f\n", get_time());
 		}
 		exit(0);
 	}
 	else{	
-		printf("CFS time: %f\n", get_time());
+		printf("Time taken normally: %f\n", get_time());
+		if (waitpid(-1, NULL, 0) == -1)
+		{
+			perror("wait: error");
+			exit(1);
+		}
 		exit(0);
 	}
-/*	
-	if (pid == -1)
-	{
-		sys_return = syscall(SYSCALL_NUMBER, getpid(), srt_val);
-	}
-	else
-	{
-		sys_return = syscall(SYSCALL_NUMBER, pid, srt_val);
-	}
-	
-	
-	if (sys_return != 0)
-	{
-		perror("Error: rtnice");
-		return -1;
-	}
-*/	
-//	printf("Run dmesg or see the file in your current directory to check the output.\n");
 	return 0;
 }
