6c71a4778f03459fa6ee5d192cddd80b61b0067f
Rohan Jain <rohan19095@iiitd.ac.in>
2020-11-12 15:29:46 +0530

    Formatting changes

--- assignment03/Q1/test.c
+++ assignment03/Q1/test.c
@@ -14,15 +14,17 @@ double get_time()
 {
 	struct timeval start_time, end_time;
 	gettimeofday(&start_time, NULL);
-	for(long int i = 0; i < LOOP_END; i++){}
+	for (long int i = 0; i < LOOP_END; i++)
+	{
+	}
 	gettimeofday(&end_time, NULL);
 	long long diff_usec = end_time.tv_usec - start_time.tv_usec;
-	if( diff_usec < 0){
+	if (diff_usec < 0)
+	{
 		diff_usec += 1000000;
 		end_time.tv_sec -= 1;
 	}
-	return (end_time.tv_sec - start_time.tv_sec + 0.000001*diff_usec);
-	
+	return (end_time.tv_sec - start_time.tv_sec + 0.000001 * diff_usec);
 }
 
 int main(int argc, char **argv)
@@ -30,19 +32,22 @@ int main(int argc, char **argv)
 	pid_t pid, fork_pid;
 	long long srt_val;
 	long int sys_return;
-	
+
 	printf("Enter pid(-1 to send pid of this process): ");
 	scanf("%d", &pid);
 	printf("Enter soft realtime requirements: ");
 	scanf("%lld", &srt_val);
-		
+
 	fork_pid = fork();
-	if(fork_pid == -1){
+	if (fork_pid == -1)
+	{
 		perror("fork: error");
 		exit(1);
 	}
-	else if(fork_pid == 0){
-		if(pid == -1){
+	else if (fork_pid == 0)
+	{
+		if (pid == -1)
+		{
 			pid = getpid();
 		}
 		// Converting seconds to nanoseconds because syscall takes nanoseconds as param
@@ -52,12 +57,14 @@ int main(int argc, char **argv)
 			perror("rtnice: error");
 			return -1;
 		}
-		else{
+		else
+		{
 			printf("Time taken with soft realtime requirements: %f\n", get_time());
 		}
 		exit(0);
 	}
-	else{	
+	else
+	{
 		printf("Time taken normally: %f\n", get_time());
 		if (waitpid(-1, NULL, 0) == -1)
 		{
