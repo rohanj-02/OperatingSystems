46c38a6fbd9adc64d1059a98a1c3b9f8b52e57d2
Rohan Jain <rohan19095@iiitd.ac.in>
2020-11-17 23:34:18 +0530

    Q5 error handling

--- quiz02/q5.c
+++ quiz02/q5.c
@@ -25,17 +25,29 @@ void *reader(void *number)
 {
 	//* Entry Section
 	// Reader will acquire the mutex lock before updating the value of current number of readers
-	pthread_mutex_lock(&mutex);
+	if (pthread_mutex_lock(&mutex) != 0)
+	{
+		perror("pthread_mutex_lock(): error");
+		exit(1);
+	}
 	reader_count++;
 
 	// If this is the first reader send a signal down to semaphore so that writer doesn't write to shm
 	if (reader_count == 1)
 	{
-		sem_wait(&writing);
+		if (sem_wait(&writing) == -1)
+		{
+			perror("sem_wait(): error");
+			exit(1);
+		}
 	}
 
 	// Remove the mutex lock so other readers can update the value of curr_reader_count
-	pthread_mutex_unlock(&mutex);
+	if (pthread_mutex_unlock(&mutex) != 0)
+	{
+		perror("pthread_mutex_unlock(): error");
+		exit(1);
+	}
 
 	//* Critical Section
 	// Attach to shm
@@ -57,27 +69,42 @@ void *reader(void *number)
 		perror("shmdt(): error");
 		exit(1);
 	}
-	// Read over
 
 	//* Exit Section
 	// Reader will acquire the mutex lock before updating the value of current number of readers
-	pthread_mutex_lock(&mutex);
+	if (pthread_mutex_lock(&mutex) != 0)
+	{
+		perror("pthread_mutex_lock(): error");
+		exit(1);
+	}
 	reader_count--;
 
 	// If this is the last reader then send a signal up to semaphore so that writer can write to shm
 	if (reader_count == 0)
 	{
-		sem_post(&writing);
+		if (sem_post(&writing) == -1)
+		{
+			perror("sem_post(): error");
+			exit(1);
+		}
 	}
 
 	// Remove the mutex lock so other readers can update the value of reader_count
-	pthread_mutex_unlock(&mutex);
+	if (pthread_mutex_unlock(&mutex) != 0)
+	{
+		perror("pthread_mutex_unlock(): error");
+		exit(1);
+	}
 }
 
 void *writer(void *number)
 {
 	//* Signal for down (Entry Section)
-	sem_wait(&writing);
+	if (sem_wait(&writing) == -1)
+	{
+		perror("sem_post(): error");
+		exit(1);
+	}
 
 	//* Critical Section
 	// Attach to shm
@@ -99,10 +126,13 @@ void *writer(void *number)
 		perror("shmdt(): error");
 		exit(1);
 	}
-	// Write over
 
 	//* Signal for Up (Exit Section)
-	sem_post(&writing);
+	if (sem_post(&writing) == -1)
+	{
+		perror("sem_post(): error");
+		exit(1);
+	}
 }
 
 int main(int argc, char *argv[])
@@ -110,8 +140,16 @@ int main(int argc, char *argv[])
 	pthread_t readers[READER_COUNT], writers[WRITER_COUNT];
 
 	// Initialise mutex and semaphores
-	pthread_mutex_init(&mutex, NULL);
-	sem_init(&writing, 0, 1);
+	if (pthread_mutex_init(&mutex, NULL) != 0)
+	{
+		perror("pthread_mutex_init(): error");
+		exit(1);
+	}
+	if (sem_init(&writing, 0, 1) == -1)
+	{
+		perror("sem_init(): error");
+		exit(1);
+	}
 
 	// Generate key and connect to the shared memory
 	if ((shm_key = ftok("q5.c", 'a')) == -1)
@@ -129,25 +167,49 @@ int main(int argc, char *argv[])
 
 	for (int i = 0; i < WRITER_COUNT; i++)
 	{
-		pthread_create(&writers[i], NULL, (void *)writer, (void *)&a[i]);
+		if (pthread_create(&writers[i], NULL, (void *)writer, (void *)&a[i]) != 0)
+		{
+			perror("pthread_create(): error");
+			// Don't exit, let other threads run.
+		}
 	}
 	for (int i = 0; i < READER_COUNT; i++)
 	{
-		pthread_create(&readers[i], NULL, (void *)reader, (void *)&a[i]);
+		if (pthread_create(&readers[i], NULL, (void *)reader, (void *)&a[i]) != 0)
+		{
+			perror("pthread_create(): error");
+			// Don't exit, let other threads run.
+		}
 	}
 
 	for (int i = 0; i < READER_COUNT; i++)
 	{
-		pthread_join(readers[i], NULL);
+		if (pthread_join(readers[i], NULL) != 0)
+		{
+			perror("pthread_join(): error");
+			// Don't exit, Let the other threads join
+		}
 	}
 	for (int i = 0; i < WRITER_COUNT; i++)
 	{
-		pthread_join(writers[i], NULL);
+		if (pthread_join(writers[i], NULL) != 0)
+		{
+			perror("pthread_join(): error");
+			// Don't exit, Let the other threads join
+		}
 	}
 
 	// Cleanup: Destroy mutex and semaphores
-	pthread_mutex_destroy(&mutex);
-	sem_destroy(&writing);
+	if (pthread_mutex_destroy(&mutex) != 0)
+	{
+		perror("pthread_mutex_destroy(): error");
+		exit(1);
+	}
+	if (sem_destroy(&writing) == -1)
+	{
+		perror("sem_destroy(): error");
+		exit(1);
+	}
 
 	return 0;
 }
