02e9c54d935afdf99d917a71e58d1554f124b98a
Rohan Jain <rohan19095@iiitd.ac.in>
2020-11-17 19:04:27 +0530

    Q1 handling escap sequences

--- quiz02/q1.c
+++ quiz02/q1.c
@@ -4,6 +4,38 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <string.h>
+#define MAX_SIZE 501
+
+int isEscape(char *in, int index)
+{
+	if (index == 0)
+	{
+		return 0;
+	}
+	if (in[index - 1] != '\\')
+	{
+		return 0;
+	}
+	switch (in[index])
+	{
+	case 'a':
+	case 'b':
+	case 'f':
+	case 'n':
+	case 'r':
+	case 't':
+	case 'v':
+	case '\\':
+	case '"':
+	case '?':
+	case '\'':
+	case '0':
+		return 1;
+	default:
+		return 0;
+	}
+}
 
 int main(void)
 {
@@ -30,23 +62,30 @@ int main(void)
 		close(pipe1[1]);
 		close(pipe2[0]);
 
-		char input;
+		char input[MAX_SIZE];
 
 		//Read from pipe
-		if (read(pipe1[0], &input, 1) == -1)
+		if (read(pipe1[0], &input, MAX_SIZE - 1) == -1)
 		{
 			perror("read(): error");
 			exit(1);
 		}
 
-		//Convert to Upper Case if alphabet
-		if (input >= 97 && input <= 122)
+		int i = 0;
+		for (i = 0; input[i] != '\0'; i++)
 		{
-			input -= 32;
+			if (!isEscape(input, i))
+			{
+				//Convert to Upper Case if alphabet
+				if (input[i] >= 97 && input[i] <= 122)
+				{
+					input[i] -= 32;
+				}
+			}
 		}
 
 		//Write upper case to pipe from child to parent
-		if (write(pipe2[1], &input, 1) == -1)
+		if (write(pipe2[1], &input, i) == -1)
 		{
 			perror("write(): error");
 			exit(1);
@@ -62,29 +101,29 @@ int main(void)
 		close(pipe1[0]);
 		close(pipe2[1]);
 
-		char input, output;
-		printf("Enter character input: "); //Comment this later
-		scanf("%c", &input);
+		char input[MAX_SIZE], output[MAX_SIZE];
+		// printf("Enter character input: "); //Comment this later
+		fgets(input, MAX_SIZE, stdin);
 
 		// Write the input to the pipe from parent to child
-		if (write(pipe1[1], &input, 1) == -1)
+		if (write(pipe1[1], &input, strlen(input)) == -1)
 		{
-			perror("read(): error");
+			perror("write(): error");
 			exit(1);
 		}
 
 		// Wait for child to convert to uppercase and write to the pipe and then read from pipe from child to parent.
-		if (read(pipe2[0], &output, 1) == -1)
+		if (read(pipe2[0], &output, MAX_SIZE - 1) == -1)
 		{
 			perror("read(): error");
 			exit(1);
 		}
-		printf("%c", output);
+		printf("%s", output);
 
 		// To wait for child process to end so no zombie
 		if (waitpid(-1, NULL, 0) == -1)
 		{
-			perror("wait: error");
+			perror("waitpid(): error");
 			exit(1);
 		}
 
