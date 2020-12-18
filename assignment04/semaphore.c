ca46224dacbdfd65931e68353fbc729894a920da
Rohan Jain <rohan19095@iiitd.ac.in>
2020-12-19 00:19:04 +0530

    Error handling

--- assignment04/semaphore.c
+++ assignment04/semaphore.c
@@ -3,13 +3,10 @@
 
 #include <pthread.h>
 #include <stdio.h>
-
-//not needed libraries
-#include <semaphore.h>
 #include <stdlib.h>
 
-#define PHIL_COUNT 25
-#define EAT_COUNT 1
+#define PHIL_COUNT 10000
+#define EAT_COUNT 10
 #define BOWL_COUNT 2
 
 typedef struct my_blocking_semaphore sem_b;
@@ -17,7 +14,6 @@ typedef struct my_blocking_semaphore sem_b;
 // TODO Error handling
 
 // Semaphore functions
-
 struct my_blocking_semaphore
 {
 	pthread_mutex_t lock;
@@ -26,41 +22,56 @@ struct my_blocking_semaphore
 	int num_waiters;
 };
 
-sem_b *init_sem_blocking(int initial_value)
+int init_sem_blocking(sem_b *my_sem, int initial_value)
 {
-	sem_b *my_sem;
-	my_sem = (sem_b *)malloc(sizeof(sem_b));
-
-	if (my_sem == NULL)
-	{
-		return NULL;
-	}
-
 	my_sem->value = initial_value;
-	pthread_mutex_init(&(my_sem->lock), NULL);
-	pthread_cond_init(&(my_sem->wait), NULL);
+	if (pthread_mutex_init(&(my_sem->lock), NULL) != 0)
+	{
+		perror("pthread_mutex_init(): error");
+		return -1;
+	}
+	if (pthread_cond_init(&(my_sem->wait), NULL) != 0)
+	{
+		perror("pthread_cond_init(): error");
+		return -1;
+	}
 	my_sem->num_waiters = 0;
-
-	return my_sem;
+	return 0;
 }
 
-void signal_blocking(sem_b *s)
+int signal_blocking(sem_b *s)
 {
-	pthread_mutex_lock(&(s->lock));
+	if (pthread_mutex_lock(&(s->lock)) != 0)
+	{
+		perror("pthread_mutex_lock(): error");
+		return -1;
+	}
 	s->value++;
 
 	if (s->value <= 0)
 	{
-		pthread_cond_signal(&(s->wait));
+		if (pthread_cond_signal(&(s->wait)) != 0)
+		{
+			perror("pthread_cond_signal(): error");
+			return -1;
+		}
 	}
 
-	pthread_mutex_unlock(&(s->lock));
-	return;
+	if (pthread_mutex_unlock(&(s->lock)) != 0)
+	{
+		perror("pthread_mutex_unlock(): error");
+		return -1;
+	}
+	return 0;
 }
 
-void wait_blocking(sem_b *s)
+int wait_blocking(sem_b *s)
 {
-	pthread_mutex_lock(&(s->lock));
+	if (pthread_mutex_lock(&(s->lock)) != 0)
+	{
+		perror("pthread_mutex_lock(): error");
+		return -1;
+	}
 	s->value--;
 
 	while (s->value < 0)
@@ -72,64 +83,73 @@ void wait_blocking(sem_b *s)
 		else
 		{
 			s->num_waiters++;
-			pthread_cond_wait(&(s->wait), &(s->lock));
+			if (pthread_cond_wait(&(s->wait), &(s->lock)) != 0)
+			{
+				perror("pthread_cond_wait(): error");
+				return -1;
+			}
+
 			s->num_waiters--;
 		}
-
-		pthread_mutex_unlock(&(s->lock));
+	}
+	if (pthread_mutex_unlock(&(s->lock)) != 0)
+	{
+		perror("pthread_mutex_unlock(): error");
+		return -1;
 	}
 
-	return;
+	return 0;
+}
+
+void print_value_blocking(sem_b *s)
+{
+	if (pthread_mutex_lock(&(s->lock)) != 0)
+	{
+		perror("pthread_mutex_lock(): error");
+		return -1;
+	}
+	printf("Value of semaphore: %d\n", s->value);
+	if (pthread_mutex_unlock(&(s->lock)) != 0)
+	{
+		perror("pthread_mutex_unlock(): error");
+		return -1;
+	}
 }
 
 // Global Variables
 
-sem_t forks[PHIL_COUNT];	   // Binary semaphore array for phil_run
-sem_t sauce_bowls[BOWL_COUNT]; // Semaphores for sauce_bowls
+sem_b forks[PHIL_COUNT];	   // Binary semaphore array for phil_run
+sem_b sauce_bowls[BOWL_COUNT]; // Semaphores for sauce_bowls
 
 // Helper functions
 
-void get_sem_at_index(sem_t *sem_arr, int index)
+void get_sem_at_index(sem_b *sem_arr, int index)
 {
-	if (sem_wait(&sem_arr[index]) == -1)
+	if (wait_blocking(&sem_arr[index]) == -1)
 	{
-		perror("sem_wait(): error");
+		printf("wait_blocking(): error");
 		exit(1);
 	}
 }
 
-void initialise_sem_array(sem_t *sem_arr, int count)
+void initialise_sem_array(sem_b *sem_arr, int count)
 {
 	for (int i = 0; i < count; i++)
 	{
-		if (sem_init(&sem_arr[i], 0, 1) == -1)
+		if (init_sem_blocking(&sem_arr[i], 1) == -1)
 		{
-			perror("sem_init(): error");
-			exit(1);
-		}
-	}
-}
-
-void destroy_sem_array(sem_t *sem_arr, int count)
-{
-	for (int i = 0; i < count; i++)
-	{
-		if (sem_destroy(&sem_arr[i]) == -1)
-		{
-			perror("sem_destroy(): error");
+			printf("init_sem_blocking(): error");
 			exit(1);
 		}
 	}
 }
 
 // Philosopher thread
-
 void *phil_run(void *number)
 {
 	int current_phil = *((int *)number);
 	int right_fork = (current_phil == PHIL_COUNT) ? 0 : current_phil;
 	int curr_eat_count = 0;
-
 	// Loop till philosopher hasn't eaten enough times
 	do
 	{
@@ -158,14 +178,10 @@ void *phil_run(void *number)
 		// Get both bowls to eat
 		for (int i = 0; i < BOWL_COUNT; i++)
 		{
-			if (sem_wait(&sauce_bowls[i]) == -1)
-			{
-				perror("sem_wait(): error");
-				exit(1);
-			}
-
+			get_sem_at_index(sauce_bowls, i);
 			printf("Philosopher %d picked up bowl %d\n", current_phil, i + 1);
 		}
+
 		// Philosopher is eating!
 		printf("Philosopher %d is eating!\n", current_phil);
 
@@ -173,21 +189,21 @@ void *phil_run(void *number)
 		// Signal that bowls are available to take
 		for (int i = BOWL_COUNT - 1; i >= 0; i--)
 		{
-			if (sem_post(&sauce_bowls[i]) == -1)
+			if (signal_blocking(&sauce_bowls[i]) == -1)
 			{
-				perror("sem_post(): error");
+				printf("signal_blocking(): error");
 				exit(1);
 			}
 		}
 		// Signal that the forks are now available to use!
-		if (sem_post(&forks[current_phil - 1]) == -1)
+		if (signal_blocking(&forks[current_phil - 1]) == -1)
 		{
-			perror("sem_post(): error");
+			printf("signal_blocking(): error");
 			exit(1);
 		}
-		if (sem_post(&forks[right_fork]) == -1)
+		if (signal_blocking(&forks[right_fork]) == -1)
 		{
-			perror("sem_post(): error");
+			printf("signal_blocking(): error");
 			exit(1);
 		}
 
@@ -231,10 +247,6 @@ int main(void)
 			// Don't exit, Let the other threads join
 		}
 	}
-
-	// Cleanup: Destroy semaphores
-	destroy_sem_array(forks, PHIL_COUNT);
-	destroy_sem_array(sauce_bowls, BOWL_COUNT);
 
 	return 0;
 }
