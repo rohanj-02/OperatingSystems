fb6f6753bdc99891d27018c2574e3a405970aaa3
Rohan Jain <rohan19095@iiitd.ac.in>
2020-12-19 13:47:30 +0530

    Bootloader print from memory

--- assignment05/Q1/bootloader.asm
+++ assignment05/Q1/bootloader.asm
@@ -1,8 +1,29 @@
-loop:
-    jmp loop 
+main:
+	; Move 0e or 10 to ah (ax high 8 bytes) to indicate teletype BIOS routine 
+	mov	ah, 0x0e
+	; Move ASCII code of character to print to al (ax lower 8 bytes)  
+	mov	al, 'H'
+	; Interrupt to print
+	int	0x10
+	mov	al, 'e'
+	int	0x10
+	mov	al, 'l'
+	int	0x10
+	int	0x10 ; 'l' is still on al, remember?
+	mov	al, 'o'
+	int	0x10
+	mov	bx, the_secret
+	add	bx, 0x7c00
+	mov	al, [ bx ]
+	int	0x10
+	jmp	$
 
-; Fill the binary till 510 bytes as placeholder after removing the size of the previous code
-times 510-($-$$) db 0
+the_secret :
+	db "X"
 
-; To recognise this as a bootable partition
-dw 0xaa55
+; Fill the binary till 510 bytes with 0s as placeholder after removing the size of the previous code
+; $ represents the current address and $$ represents the start address
+times	510-($-$$) db 0
+
+; To make BIOS recognise this as a bootsector
+dw	0xaa55
