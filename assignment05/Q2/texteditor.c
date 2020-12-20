58023cfdd009fdc33ecb88dc545d188fb8d36fba
Rohan Jain <rohan19095@iiitd.ac.in>
2020-12-20 14:40:49 +0530

    File read

--- assignment05/Q2/texteditor.c
+++ assignment05/Q2/texteditor.c
@@ -4,13 +4,61 @@
 #include <string.h>
 
 #define str_equal(a, b) (strcmp(a, b) == 0)
+#define MAX_BUFFER_SIZE 1024
 
-int check_advisory_locks(char *filename)
+void print_error(char *err_msg)
+{
+	perror(err_msg);
+	exit(1);
+}
+
+int check_advisory_locks(FILE *fptr)
 {
 }
 
-int read_to_file(char *filename)
+int wait_for_close()
 {
+	while (1)
+	{
+		printf("Enter q to quit the program: ");
+		char buffer[10];
+		scanf("%s", buffer);
+		if (buffer[0] == 'q')
+		{
+			return 0;
+		}
+	}
+}
+
+int read_from_file(char *filename)
+{
+	FILE *fptr;
+	if ((fptr = fopen(filename, "r")) == NULL)
+	{
+		print_error("fopen(): error");
+	}
+
+	check_advisory_locks(fptr); // get advisory lock
+
+	// What does getc return on error?
+	char ch = getc(fptr);
+	while (ch != EOF)
+	{
+		printf("%c", ch);
+		ch = getc(fptr);
+	}
+
+	printf("\n");
+	wait_for_close();
+	// wait for input
+
+	if (fclose(fptr) != 0)
+	{
+		print_error("fclose(): error");
+	}
+
+	// release lock
+	return 0;
 }
 
 int write_to_file(char *filename)
@@ -37,6 +85,7 @@ int main(int argc, char **argv)
 	if (str_equal(argv[1], "read"))
 	{
 		printf("Read mode\n");
+		read_from_file(argv[2]);
 	}
 	else if (str_equal(argv[1], "write"))
 	{
